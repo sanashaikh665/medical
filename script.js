4976db4f8ddbcd09b1ef659c93293fd56da88551
sanashaikh665 <156634377+sanashaikh665@users.noreply.github.com>
2024-11-08 20:34:57 +0530

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
- let menu = document.querySelector('#menu-icon');
+let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbarr');
+let navItems = document.querySelectorAll('.navbarr a');  
 
-menu.onclick = () =>
-{
+menu.onclick = () => {
   menu.classList.toggle('bi-x');
   navbar.classList.toggle('open');
-}
+};
+
+
+navItems.forEach(item => {
+  item.onclick = () => {
+    navbar.classList.remove('open'); 
+    menu.classList.remove('bi-x'); 
+  };
+});
